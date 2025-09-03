cf84f956e27b190c5d96358d381824fab85119be
Ravichandra Vajinepalli <129595506+Ravichandra-DevOps@users.noreply.github.com>
2025-09-03 09:01:04 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',           // explicitly set root
   build: {
-    outDir: 'dist',
+    outDir: 'dist',    // output to /dist
   },
 });
